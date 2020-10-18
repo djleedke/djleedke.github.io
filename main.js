75586d14b2c70a9244aace81f98190f7c9b38c2e
Doug L <leedke@gmail.com>
2020-10-18 15:55:27 -0500

    Gifs now only play on hover, more CSS added to overlay

---
+++ main.js
@@ -0,0 +1,27 @@
+
+$('#tweetry-dish').hover(
+    function(){
+        $(this).find('img').attr('src', 'https://user-images.githubusercontent.com/33850990/88748945-e2569700-d117-11ea-8278-4c2492034b2a.gif');
+    },
+    function(){
+        $(this).find('img').attr('src', 'https://user-images.githubusercontent.com/33850990/96378681-65095400-1153-11eb-9411-39f80df8ba14.png');
+    }
+);
+
+$('#chat-code').hover(
+    function(){
+        $(this).find('img').attr('src', 'https://user-images.githubusercontent.com/33850990/88751848-1f258c80-d11e-11ea-8e2d-0ee1dc42bed7.gif');
+    },
+    function(){
+        $(this).find('img').attr('src', 'https://user-images.githubusercontent.com/33850990/96379977-1b216d80-1155-11eb-966d-05ef499e7fab.png');
+    }
+);
+
+$('#ping-pi').hover(
+    function(){
+        $(this).find('img').attr('src', 'https://user-images.githubusercontent.com/33850990/95130173-caa41c00-0721-11eb-832d-cab79e4f5128.gif');
+    },
+    function(){
+        $(this).find('img').attr('src', 'https://user-images.githubusercontent.com/33850990/96381950-d0542580-1155-11eb-9983-bf4c0e293048.png');
+    }
+);
